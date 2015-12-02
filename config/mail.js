ff6565cece30785807d809a9609a383814debf73
Mate Šimović <mate.simovic@fer.hr>
2015-12-02 16:17:09 +0100

    Vratio službenu mail adresu bubbles društvene mreže.

--- config/mail.js
+++ config/mail.js
@@ -5,8 +5,8 @@ module.exports={
             //moj email bubbbles.mislav@gmail.com
             service: 'Gmail',
             auth: {
-                user: 'mate.simovic.orange@gmail.com', //dodati svoj mail
-                pass: 'drustvena-mreza'   //dodati svoju sifru
+                user: 'bubbles.notification', //dodati svoj mail
+                pass: 'Dr(!*Ezk@R48'   //dodati svoju sifru
             }
         });
         var mailOptions = {
