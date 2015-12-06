91acc70f8a16cf8010e6e617f41219a827ef5269
zeljko-b <zeljko.baranek@fer.hr>
2015-12-06 18:22:47 +0100

    ispravljene neke greške u header, footer i ostalima pronađene na W3C validatoru

--- public/javascripts/navigator.js
+++ public/javascripts/navigator.js
@@ -26,38 +26,42 @@ function clearSearchResults() {
 	var sr = document.getElementById('search-results');
 	sr.innerHTML = '';
 	sr.style='display:none';
-	sb.parent().removeClass('btn-danger');
-	sb.addClass('glyphicon glyphicon-search');
 }
 
 $('#search-form').on('submit', function(event){
 	event.preventDefault();
 	
-	var sb = $('#search-form button span.glyphicon');
-	sb.removeClass('glyphicon-search');
-	sb.addClass('glyphicon-refresh');
-	sb.addClass('gly-spin');
-	
-	$.get('/search?query='+ $('#search-term').val(), function (data, status) {
-		var sr = document.getElementById('search-results');
-		sr.innerHTML = '<li class="dropdown-header"><span class="glyphicon glyphicon-user"> Contacts<li><li role="separator" class="divider"></li>';
-		$.each(data.users, function() {
-			itemLi = document.createElement('li');
-			itemA = document.createElement('a');
-			itemA.href = '/profile/view/'+ this.username;
-			itemA.innerHTML = this.first_name +' '+ this.last_name;
-			itemLi.appendChild(itemA);
-			sr.appendChild(itemLi);
-		});
+	var st = $('#search-term');
+	if (st.val() == '') {
+		st.focus();
+	}
+	else {
+		var sr = $('#search-results');
+		var sb = $('#search-form button span.glyphicon');
+		sb.removeClass('glyphicon-search');
+		sb.addClass('glyphicon-refresh');
+		sb.addClass('gly-spin');
+		
+		$.get('/search?query='+ st.val(), function (data, status) {
+			sr.html('<li class="dropdown-header"><span class="glyphicon glyphicon-user"> Contacts<li><li role="separator" class="divider"></li>');
+			$.each(data.users, function() {
+				itemLi = document.createElement('li');
+				itemA = document.createElement('a');
+				itemA.href = '/profile/view/'+ this.username;
+				itemA.innerHTML = this.first_name +' '+ this.last_name;
+				itemLi.appendChild(itemA);
+				sr.append(itemLi);
+			});
+				
+			sb.removeClass('gly-spin');
+			sb.removeClass('glyphicon-refresh');
+			sb.addClass('glyphicon glyphicon-search');
 			
-		sb.removeClass('gly-spin');
-		sb.removeClass('glyphicon-refresh');
-		sb.addClass('glyphicon glyphicon-search');
-		
-		sr.style='display:block';
-		
-		sb.on('click', clearSearchResults);
-	});
+			$('#search-results').css('display', 'block');
+			
+			sb.on('click', clearSearchResults);
+		});
+	}
 });
 
 $(document).click(function(event) { 
@@ -67,6 +71,7 @@ $(document).click(function(event) {
         }
     }        
 })
+
 
 $(window).on('hashchange', loadPage);
 $(document).ready(chechHash);
