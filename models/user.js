e7098d684f66485557588a51c99c310672732432
Gordan Graberec <g.graberec@hotmail.com>
2015-12-07 19:23:28 +0100

    Jos malo pa...

--- models/user.js
+++ models/user.js
@@ -1,18 +1,42 @@
 
 var db = require('../config/db');
+var checkit = require('checkit');
+var Promise = require('bluebird');
 
-require('./country');
+var Country = require('./country');
 require('./relationship_status');
 require('./bubble');
 
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
 
 var User = db.Model.extend({
     tableName: 'user',
     hasTimestamps : true,
     country : function () { return this.belongsTo('Country'); },
     relationshipStatus : function() { return this.belongsTo('RelationshipStatus'); },
-    bubbles : function() { return this.hasMany('Bubble'); }
+    bubbles : function() { return this.hasMany('Bubble');},
+
+    initialize : function() {
+        this.on('saving', this.validateAndHash, this);
+    },
+
+    validateAndHash : function(model, attributes, options) {
+        var checkUser = checkit(rules).maybe(passwordRules, function(input) {
+            return model.hasChanged('password_hash');
+        });
+
+        return checkUser.run(model.attributes).then(Promise.method(function () {
+            if(model.hasChanged('password_hash')) {
+                model.attributes
+            }
+        }));
+    },
+
+    hash : function() {
+        return bcrypt.genSaltAsync(10).then(function (result) {
+            return
+        })
+    }
 });
 
 User.validPassword = function(pass1,pass2) {
@@ -24,3 +48,84 @@ User.generateHash = function(password) {
 };
 
 module.exports=db.model('User', User);
+
+var passwordRules = {
+    password_hash: [
+        {
+            rule: 'required',
+            message: 'Password is required'
+        },
+        {
+            rule: 'minLength:8',
+            message: 'Password must be at least 8 characters in length.'
+        },
+        {
+            rule: 'maxLength:30',
+            message: 'Password must be at most 30 characters in length.'
+        },
+        {
+            rule: 'alphaDash',
+            message: 'Password must consist of alphanumerics, dashes and underscores.'
+        }
+    ]
+};
+
+var rules = {
+    username: [
+        {
+            rule: 'required',
+            message: 'Username is required.'
+        },
+        {
+            rule: 'minLength:3',
+            message: 'Username must be at least 3 characters in length.'
+        },
+        {
+            rule: 'maxLength:30',
+            message: 'Username must be at most 30 characters in length'
+        },
+        {
+            rule: 'alphaDash',
+            message: 'Username must consist of alphanumerics, dashes and underscores.'
+        },
+        {
+            rule: function(username) {
+                return User.where({username: username}).fetch().then(function(user) {
+                    if(user) throw new Error('Username already exists.')
+                })
+            }
+        }
+
+    ],
+    email: [
+        {
+            rule: 'required',
+            message: 'Email is required.'
+        },
+        {
+            rule: 'email',
+            message: 'Invalid email format.'
+        },
+        {
+            rule: function(email) {
+                return User.where({email: email}).fetch().then(function(user) {
+                    if(user) {
+                        throw new Error('Email already exists.');
+                    }
+                })
+            }
+        }
+
+    ],
+    country_id: [
+        Promise.method(function (country_id) {
+            if(country_id) {
+                Country.where({id: country_id}).fetch().then(function(country) {
+                    if(!country){
+                        throw new Error('Country does not exist.');
+                    }
+                });
+            }
+        })
+    ]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
+
 var express = require('express');
 var router = express.Router();
-var bcrypt = require('bcrypt-nodejs');
+var Mail=require('../config/mail');
+var checkit = require('checkit');
+
 var User = require('../models/user');
 var Country = require('../models/country');
-var Mail=require('../config/mail');
-
 
 module.exports = function(passport){
 
@@ -68,48 +69,24 @@ module.exports = function(passport){
     router.post('/sign-up', function(req, res, next) {
         var form = req.body;
         var country_id;
-        Promise.all([
-            Promise.resolve(!/^[a-zA-Z][a-zA-Z0-9_-]{2,29}$/.test(form.username) ? 'Username must begin with an alphabetic character, be between 3 and 30 characters in length, contain only alphanumerics, underscores and hyphens' : null),
-            Promise.resolve(!/^[A-Za-z0-9_-]{8,30}$/.test(form.password) ? 'Password must be between 8 and 30 characters in length, contain only alphanumerics, underscores and hyphens' : null),
-            Promise.resolve(!/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(form.email) ? 'invalid email' : null),
-            User.where({username: form.username}).fetch().then(function (user) {
-                return Promise.resolve(user ? 'Username already exists' : null);
-            }),
-            User.where({email: form.email}).fetch().then(function (user) {
-                return Promise.resolve(user ? 'E-mail already exists' : null);
-            }),
-            Country.where({name: form.country}).fetch().then(function (country) {
-                coutry_id = country ? country.id : null;
-                return Promise.resolve(form.country ? (country ? null : 'Country does not exist') : null);
+        Country.where({name: form.country}).fetch().then(function (country) {
+            country_id = country ? country.id : null;
+        }).then(function() {
+            var user = User.forge({
+                username : form.username,
+                email : form.email,
+                password_hash : form.password,
+                first_name : form.firstName || null,
+                last_name : form.lastName || null,
+                middle_name : form.middleName || null,
+                address : form.address || null,
+                city : form.city || null,
+                country_id : country_id
+            }).save().then(function (user) {
+                Mail.sendVerificationEmail(form.email, "localhost:8080/emailverification?id=" + user.id + "&hash=" + user.password_hash);
+                res.render('sign-up-successful.ejs', {title: 'Confirm account', data: form});
             })
-        ]).then(function(errorMessages) {
-            var error = [];
-            errorMessages.forEach(function (message) {
-                if(message) {
-                    error.push(message);
-                }
-            });
-            if(error.length === 0) {
-                var hash = User.generateHash(form.password);
-                User.forge({
-                    username : form.username,
-                    email : form.email,
-                    password_hash : hash,
-                    confirmed : false,
-                    first_name : form.firstName || null,
-                    last_name : form.lastName || null,
-                    middle_name : form.middleName || null,
-                    address : form.address || null,
-                    city : form.city || null,
-                    country_id : country_id
-                }).save().then(function (user) {
-                    Mail.sendVerificationEmail(form.email, "localhost:8080/emailverification?id=" + user.id + "&hash=" + hash);
-                    res.render('sign-up-successful.ejs', {title: 'Confirm account', data: form});
-                })
-            } else {
-                res.render('index', {title: 'Sign up', signUp: true, registerError: error, registrationInput: form});
-            }
-        }).catch(function (err) {
+        }).catch(checkit.Error, function(err) {
             console.log(err);
         });
     });
