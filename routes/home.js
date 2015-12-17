e1bb4f0d0c0ee61f4cb9af73dce053ca9d9b9a41
Gordan Graberec <g.graberec@hotmail.com>
2015-12-17 22:39:15 +0100

    /home/feed zasad samo dohvaca od prijatelja

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,9 @@
  * Created by Mislav on 11/27/2015.
  */
 var User = require('../models/user');
+var Bubble = require('../models/bubble');
+var Content = require('../models/content');
+var Privilege = require('../models/privilege')
 
 var express = require('express');
 var router = express.Router();
@@ -12,6 +15,23 @@ router.all('*', requireAuthentication);
 
 router.get('/homepage', function(req, res, next) {
     res.render('homepage.ejs', {title: 'Home', user: req.user.toJSON()});
+});
+
+router.get('/feed', function(req, res) {
+    Privilege.query(function (qb) {
+        qb.join('bubble', 'privilege.permitter_id', 'bubble.user_id')
+            .join('content', 'content.bubble_id', 'bubble.id')
+            .leftJoin('like', 'content.id', 'like.content_id')
+            .where('privilege.permittee_id', req.user.id)
+            .groupBy('content.bubble_id', 'content.id', 'content.created_at', 'content.updated_at', 'content.title', 'content.content', 'content.description')
+            .columns('content.*')
+            .count('like.user_id as likes')
+            .orderBy('content.created_at', 'DESC');
+    }).fetchAll().then(function (contents) {
+        res.json({contents: contents});
+    }).catch(function (error) {
+        console.log(error.stack);
+    });
 });
 
 module.exports=router;
