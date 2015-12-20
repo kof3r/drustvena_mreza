fe27b810107fe4923d9ad1b8c340214a14f1d633
Gordan Graberec <g.graberec@hotmail.com>
2015-12-20 13:41:12 +0100

    iLike

--- routes/home.js
+++ routes/home.js
@@ -18,23 +18,33 @@ router.get('/homepage', function(req, res, next) {
 });
 
 router.get('/feed', function(req, res) {
+    var user = req.user;
+
     Privilege.query(function (qb) {
         qb.join('bubble', 'privilege.permitter_id', 'bubble.user_id')
             .join('content', 'content.bubble_id', 'bubble.id')
             .join('user', 'bubble.user_id', 'user.id')
             .leftJoin('like', 'content.id', 'like.content_id')
+            .leftJoin('like as myLike', function() {
+                this.on('content.id', 'myLike.content_id').andOn('myLike.user_id', user.get('id'));
+            })
             .where('privilege.permittee_id', req.user.id)
             .columns('content.*', 'user.username')
             .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description', 'username')
-            .count('like.user_id as likes').union(function () {
+            .count('like.user_id as likes')
+            .count('myLike.user_id as iLike').union(function () {
                 this.from('bubble')
                     .join('content', 'content.bubble_id', 'bubble.id')
                     .join('user', 'bubble.user_id', 'user.id')
                     .leftJoin('like', 'content.id', 'like.content_id')
+                    .leftJoin('like as myLike', function() {
+                        this.on('content.id', 'myLike.content_id').andOn('myLike.user_id', user.get('id'));
+                    })
                     .where('bubble.user_id', req.user.id)
                     .columns('content.*', 'user.username')
                     .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description', 'username')
                     .count('like.user_id as likes')
+                    .count('myLike.user_id as iLike')
                     .orderBy('created_at', 'DESC');
             })
     }).fetchAll().then(function (contents) {
