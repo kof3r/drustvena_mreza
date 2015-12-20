446c6cce637eb5b764079cc7a03413a0d832ed68
Gordan Graberec <g.graberec@hotmail.com>
2015-12-20 13:06:57 +0100

    /home/feed vraća username uz svaki content

--- routes/home.js
+++ routes/home.js
@@ -21,17 +21,19 @@ router.get('/feed', function(req, res) {
     Privilege.query(function (qb) {
         qb.join('bubble', 'privilege.permitter_id', 'bubble.user_id')
             .join('content', 'content.bubble_id', 'bubble.id')
+            .join('user', 'bubble.user_id', 'user.id')
             .leftJoin('like', 'content.id', 'like.content_id')
             .where('privilege.permittee_id', req.user.id)
-            .columns('content.*')
-            .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description')
+            .columns('content.*', 'user.username')
+            .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description', 'username')
             .count('like.user_id as likes').union(function () {
                 this.from('bubble')
                     .join('content', 'content.bubble_id', 'bubble.id')
+                    .join('user', 'bubble.user_id', 'user.id')
                     .leftJoin('like', 'content.id', 'like.content_id')
                     .where('bubble.user_id', req.user.id)
-                    .columns('content.*')
-                    .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description')
+                    .columns('content.*', 'user.username')
+                    .groupBy('bubble_id', 'id', 'created_at', 'updated_at', 'title', 'content', 'description', 'username')
                     .count('like.user_id as likes')
                     .orderBy('created_at', 'DESC');
             })
