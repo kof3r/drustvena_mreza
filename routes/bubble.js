c191a18c3ae2e26d1d736ab5bc0dca96b7d81075
Gordan Graberec <g.graberec@hotmail.com>
2015-12-21 19:59:40 +0100

    /bubble dorađen, api nadopunjen

--- routes/bubble.js
+++ routes/bubble.js
@@ -14,15 +14,17 @@ router.all("*", require('../utils/authentication'));
 
 router.post('/create', function(req, res) {
     var form = req.body;
+    var user = req.user;
+
     Bubble.forge({
-        user_id: req.user.get('id'),
+        user_id: user.get('id'),
         bubble_type_id: 3,
         title: form.title,
         description: form.description
     }).save().then(function (bubble) {
-        res.end();
+        res.json(bubble);
     }).catch(ValidationError, function (error) {
-        res.json({errors: error.messages});
+        res.status(400).json({errors: error.messages});
     })
 });
 
