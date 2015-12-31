40f13e88f2c7b0b6deed5f130cd58cc9e4a7feed
zeljko-b <zeljko.baranek@fer.hr>
2015-12-31 13:49:20 +0100

    VAŽNO!!! promjena porta s 8080 na 80 (pristup putem http://localhost), zakomentiran seed za članove tima, promijenjen link za aktibaciju na www.bubbles.com.hr

--- models/user.js
+++ models/user.js
@@ -73,7 +73,7 @@ var User = orm.Model.extend({
     }),
 
     sendConfirmationMail: Promise.method(function() {
-        Mail.sendVerificationEmail(this.get('email'), "vukilab.com:8080/emailverification?id=" + this.get('id') + "&hash=" + this.get('password_hash'));
+        Mail.sendVerificationEmail(this.get('email'), "http://www.bubbles.com.hr/emailverification?id=" + this.get('id') + "&hash=" + this.get('password_hash'));
     }),
 
     format: function(attributes) {
