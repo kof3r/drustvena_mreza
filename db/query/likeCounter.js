025942b3e871cfaaa2ffa81b907dc1249bc484c9
Gordan Graberec <g.graberec@hotmail.com>
2016-01-05 01:55:38 +0100

    api/home/feed upit optimiran

---
+++ db/query/likeCounter.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Gordan on 5.1.2016..
+ */
+
+var knex = require('../../config/knex');
+
+module.exports = function (contentQuery, user_id) {
+    return contentQuery.leftJoin(likeDislikeOJ('likeDislikeOJ'), 'content.id', 'likeDislikeOJ.content_id')
+        .leftJoin(likeDislikeOJ('iLikeDislikeOJ'), function() {
+            this.on(knex.raw('content.id = iLikeDislikeOJ.content_id and ((likeDislikeOJ.liked = iLikeDislikeOJ.liked and iLikeDislikeOJ.liked = ?) or (likeDislikeOJ.disliked = iLikeDislikeOJ.disliked and iLikeDislikeOJ.disliked = ?))', [user_id, user_id]))
+        })
+        .groupBy('user.id', 'user.username', 'user.avatar', 'content.id', 'content.created_at', 'content.updated_at', 'content.title', 'content.content')
+        .count('likeDislikeOJ.liked as likes')
+        .count('likeDislikeOJ.disliked as dislikes')
+        .count('iLikeDislikeOJ.liked as iLike')
+        .count('iLikeDislikeOJ.disliked as iDislike')
+}
+
+function likeDislikeOJ(alias) {
+    return function() {
+        this.union(function() {
+            this.from('like')
+                .joinRaw('left join dislike on like.content_id = dislike.content_id and like.user_id = dislike.user_id')
+                .column('like.content_id', 'like.user_id as liked', 'dislike.user_id as disliked')
+        }).union(function () {
+            this.from('like')
+                .joinRaw('right join dislike on like.content_id = dislike.content_id and like.user_id = dislike.user_id')
+                .column('dislike.content_id', 'like.user_id as liked', 'dislike.user_id as disliked')
+        }).as(alias);
+    }
+}
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@
  * Created by Mislav on 11/27/2015.
  */
 
+var knex = require('../config/knex');
 var Promise = require('bluebird');
 
 var User = require('../models/user');
@@ -17,74 +18,28 @@ var requireAuthentication = require('../utils/authentication');
 router.all('*', requireAuthentication);
 
 router.get('/feed', function(req, res) {
-    var user = req.user;
+    var user_id = req.user.id;
 
-    Promise.join(
-        getContents(),
-        getLikesOrDislikes('like', 'myLike', 'likes', 'iLike'),
-        getLikesOrDislikes('dislike', 'myDislike', 'dislikes', 'iDislike'),
-        function (_contents, _likes, _dislikes){
-            var contents = _contents.toJSON();
-            var likes = _likes.toJSON();
-            var dislikes = _dislikes.toJSON();
-            for(var i = 0; i < _contents.length; i++) {
-                contents[i]['likes'] = likes[i].count;
-                contents[i]['dislikes'] = dislikes[i].count;
-                contents[i]['iLike'] = likes[i].bool;
-                contents[i]['iDislike'] = dislikes[i].bool;
-            }
+    var contentQuery = knex.from(function() {
+        this.union(function () {
+            this.from('privilege')
+                .where('permittee_id', user_id)
+                .column('permitter_id as id')
+        }).union(function () {
+            this.select(knex.raw('? as id', [user_id]))
+        }).as('friendsAndMe')
+    }).joinRaw('join bubble on bubble.user_id = friendsAndMe.id and bubble_type_id <> 2')
+        .joinRaw('join user on bubble.user_id = user.id')
+        .joinRaw('join content on bubble.id = content.bubble_id');
 
+    var appendLikes = require('../db/query/likeCounter');
+
+    appendLikes(contentQuery, user_id)
+        .column('user.id as user_id', 'username', 'avatar','content.bubble_id', 'content.id', 'content.content_type_id', 'content.created_at', 'content.updated_at', 'content.title', 'content.content', 'content.description')
+        .orderBy('created_at', 'DESC')
+        .then(function (contents) {
             res.json({contents: contents});
-    }).catch(function (error) {
-        console.log(error.stack);
-    });
-
-    function getContents() {
-        return Privilege.query(function (qb) {
-            qb.join('bubble', 'privilege.permitter_id', 'bubble.user_id')
-                .join('content', 'content.bubble_id', 'bubble.id')
-                .join('user', 'bubble.user_id', 'user.id')
-                .where('privilege.permittee_id', user.get('id'))
-                .columns('content.*', 'username', 'avatar', 'user_id').union(function () {
-                this.from('bubble')
-                    .join('content', 'content.bubble_id', 'bubble.id')
-                    .join('user', 'bubble.user_id', 'user.id')
-                    .where('user.id', user.get('id'))
-                    .columns('content.*', 'username', 'avatar', 'user.id as user_id')
-                    .orderBy('created_at', 'DESC');
-            })
-        }).fetchAll();
-    }
-
-    function getLikesOrDislikes(tableName) {
-        return Privilege.query(function (qb) {
-            qb.join('bubble', 'privilege.permitter_id', 'bubble.user_id')
-                .join('content', 'content.bubble_id', 'bubble.id')
-                .leftJoin(tableName, tableName + '.content_id', 'content.id')
-                .leftJoin(tableName + ' as alias', function () {
-                    this.on('alias.content_id', tableName + '.content_id').andOn('alias.user_id', tableName + '.user_id').andOn('alias.user_id', user.get('id'))
-                })
-                .where('privilege.permittee_id', user.get('id'))
-                .groupBy('content.bubble_id', 'content.id', 'content.content_type_id', 'content.created_at', 'content.title', 'content.content')
-                .columns('content.created_at')
-                .count(tableName + '.user_id as count')
-                .count('alias.user_id as bool').union(function () {
-                this.from('bubble')
-                    .join('content', 'content.bubble_id', 'bubble.id')
-                    .join('user', 'bubble.user_id', 'user.id')
-                    .leftJoin(tableName, tableName + '.content_id', 'content.id')
-                    .leftJoin(tableName + ' as alias', function () {
-                        this.on('alias.content_id', tableName + '.content_id').andOn('alias.user_id', tableName + '.user_id').andOn('alias.user_id', user.get('id'))
-                    })
-                    .where('user.id', user.get('id'))
-                    .groupBy('content.bubble_id', 'content.id', 'content.content_type_id', 'content.created_at', 'content.title', 'content.content')
-                    .columns('content.created_at')
-                    .count(tableName + '.user_id as count')
-                    .count('alias.user_id as bool')
-                    .orderBy('created_at', 'DESC');
-            })
-        }).fetchAll();
-    }
+        })
 });
 
 module.exports=router;
